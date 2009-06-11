178cd690200faec70d76c33021c56260ec9121f7
WolaS <jcpenche@gmail.com>
2009-06-11 11:53:23 +0200

    speed up V1

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,4 @@
 module UsersHelper
-  def widget_for user, surname = false
-    render :partial => 'users/widget', :locals => {:user => user, :surname => surname}
-  end
-
   def admin?
     current_admin
   end
