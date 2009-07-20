ad070cf918de317d6591021db92609ab0c6b08c4
Jake <jcpenche@gmail.com>
2009-07-20 11:41:46 +0200

    spin changed to god

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,15 @@
 default_run_options[:pty] = true
 set :spinner_user, nil
+set :runner, :yruser
 set :application, "addressbook"
 set :repository,  "git@github.com:wolas/addressbook.git"
+set :user, :yruser
+set :passphrase, 'yrpassword'
+set :deploy_to, "/home/yruser/apps/#{application}"
+set :branch, "master"
+
 set :scm, :git
 set :scm_username, "wolas"
-set :user, :yruser
-set :deploy_to, "/home/yruser/apps/#{application}"
 set :scm_passphrase, "deploypassword"
-set :branch, "master"
 
 server "152.146.39.15", :app, :web, :db, :primary => true
