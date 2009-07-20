8305d95424935e91faf1ce2d00ac18bd6a72699e
Jake <jcpenche@gmail.com>
2009-07-20 11:46:53 +0200

    spin changed to god

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,6 @@ set :runner, :yruser
 set :application, "addressbook"
 set :repository,  "git@github.com:wolas/addressbook.git"
 set :user, :yruser
-set :passphrase, 'yrpassword'
 set :deploy_to, "/home/yruser/apps/#{application}"
 set :branch, "master"
 
@@ -13,3 +12,8 @@ set :scm_username, "wolas"
 set :scm_passphrase, "deploypassword"
 
 server "152.146.39.15", :app, :web, :db, :primary => true
+
+after "deploy:update_code", :fix_script_perms
+task :fix_script_perms do
+  run "chmod 755 #{latest_release}/script/spin"
+end
