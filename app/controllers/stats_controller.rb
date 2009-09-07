985a2b0f1fcae7104f60c7ef819930440ac7663b
Jake Carbo <wolas@cath.emea.corp.yr.com>
2009-09-07 19:53:57 +0200

    added graphs for stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,8 +2,21 @@ class StatsController < ApplicationController
   
   # GET /stats
   def index
-    @stats = Stat.find(:all)
-    @stats = @stats.group_by {|stat| stat.time.to_date }
-    @stats = @stats.map {|date, stats| [date.strftime("%d-%m"), stats.count]}
+    @stats = Stat.all
+    @stats = make_stats @stats
+  end
+  
+  def change_dates
+    dates = ((params[:range].to_i.days.ago.to_date)..Date.today).to_a
+    @stats = Stat.all.select{|stat| dates.include? stat.time.to_date }
+    @stats = make_stats @stats
+    render :partial => 'graph', :locals => {:stats => @stats}
+  end
+  
+  protected
+  
+  def make_stats stats
+    @stats = @stats.group_by {|stat| stat.time.to_date }.sort
+    @stats = @stats.map {|date, stats| [date.strftime("%d %b %Y"), stats.count]}
   end
 end
